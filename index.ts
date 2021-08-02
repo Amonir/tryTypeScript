90b039e9b6b863c19d99a0c651765eceab528a0e
YahyaQandel <yqandel@cloudinn.net>
2021-08-02 10:06:12 +0200

    fix order of output , you should add any line coming after the prompts and inside the async function

--- index.ts
+++ index.ts
@@ -14,44 +14,15 @@ let path = 'users.txt';
 console.log('Welcome to our Notes app');
 
 const prompts = require('prompts');
-function toto():void {
 
-
-const res = (async () => {
+(async () => {
     const response = await prompts({
       type: 'number',
       name: 'value',
-      message: 'How old are you?',
-      validate: value => value < 18 ? `Nightclub is 18+ only` : true
+      message: 'How old are you?'
     });
   
     console.log(response); // => { value: 24 }
+    console.log('end of file')
   })();
-  }
 
-console.log('end of file')
-export{}
-// rl.question('Are you a registered user ? [y/n] ', (answer) => {
-// 	switch (answer.toLowerCase()) {
-// 		case 'y':
-// 			// enter user name pass
-// 			break;
-// 		case 'n':
-// 			console.log('Sorry! lets register  :(');
-// 			rl.question('Please enter user name. ', (enteredUsername) => {
-// 				username = enteredUsername;
-// 				rl.question('Please enter password. ', (enteredPassword) => {
-// 					password = enteredPassword;
-// 					let userString = username + '/' + password + ' ';
-// 					fs.appendFile('user.text', userString, (err) => {
-// 						if (err) throw err;
-// 						console.log('user added');
-// 					});
-// 				});
-// 			});
-// 		// 	break;
-// 		// default:
-// 		// 	console.log('Invalid answer!');
-// 	}
-// 	rl.close();
-// });
