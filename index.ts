fd0399ba81ad61c7e49f0ba979d269605078cf95
amonir <amonir@cloudinn.net>
2021-08-22 16:18:40 +0200

    last commit Co-authored-by: Yahya <Yahya.Qandel@gmail.com>

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import { type } from "node:os";
+import { couldStartTrivia } from "typescript";
+
 const prompts = require('prompts');
 const fs = require('fs');
 const readline = require('readline');
@@ -5,8 +8,8 @@ let username: string;
 let password: string;
 // let path = 'users.txt';
 let firstSelection: { [key: string]: string };
-let savedUserInfo: { [key: string]: string };
 let userLoginInfo: { [key: string]: string };
+let LoggedUserSelection: { [key: string]: string };
 
 const selectionQuestions = [
 	{
@@ -17,6 +20,19 @@ const selectionQuestions = [
 			{ title: 'Login as a guest', value: 'guest' },
 			{ title: 'Login as a registered user', value: 'login' },
 			{ title: 'Register new user ', value: 'register' },
+		],
+	},
+];
+
+const afterLogin = [
+	{
+		type: 'select',
+		name: 'selection',
+		message: 'What do you want to do today?',
+		choices: [
+			{ title: 'Show current tasks', value: 'display' },
+			{ title: 'Add Task', value: 'add' },
+			{ title: 'remove tasks ', value: 'remove' },
 		],
 	},
 ];
@@ -52,6 +68,11 @@ async function getUserSelection(): Promise<any> {
 	return response;
 }
 
+async function getLoggedUserSelection(): Promise<any> {
+	const response = await prompts(afterLogin);
+	return response;
+}
+
 async function getRegistration(): Promise<any> {
 	const response = await prompts(register);
 	return response;
@@ -63,8 +84,8 @@ async function getLogin(): Promise<any> {
 }
 
 // solution 2 didn't work
-async function readLineFromFile(filePath: string): Promise<any>{
-	return new Promise(resolve => {
+async function readLineFromFile(filePath: string): Promise<any> {
+	return new Promise((resolve) => {
 		const readInterface = readline.createInterface({
 			input: fs.createReadStream(filePath),
 			// output: process.stdout,
@@ -72,11 +93,10 @@ async function readLineFromFile(filePath: string): Promise<any>{
 		});
 		let lines: Array<string> = [];
 		readInterface.on('line', (line: any) => {
-			lines.push(line)
-			resolve(lines)
+			lines.push(line);
+			resolve(lines);
 		});
 	});
-	
 }
 // for loop didn't work
 // for await (const line of readInterface) {
@@ -89,6 +109,7 @@ console.log('Welcome to our Notes app');
 
 getUserSelection().then((selection: any) => {
 	firstSelection = selection;
+	console.log(typeof(firstSelection))
 	switch (firstSelection['selection']) {
 		case 'guest': {
 			console.log('activate guest app!');
@@ -100,35 +121,13 @@ getUserSelection().then((selection: any) => {
 				userLoginInfo = loginInfo;
 				console.log('Entered Username: ', userLoginInfo['username']);
 
-				// solution 3 didn't work
-				// const readInterface = readline.createInterface({
-				// 	input: fs.createReadStream('users.txt'),
-				// 	// output: process.stdout,
-				// 	console: false,
-				// });
-				// readInterface.on('line', (line: any) => {
-				// 	savedUserInfo = line;
-				// 	// console.log(line,'====', savedUserInfo,'compared to ', userLoginInfo)
-				// 	console.log(userLoginInfo['username'], savedUserInfo['username']);
-				// 	if (userLoginInfo['username'] == savedUserInfo['username'] && userLoginInfo['password'] == savedUserInfo['password']) {
-				// 		console.log('welcome home', userLoginInfo['username']);
-				// 		const currentUser = userLoginInfo['username'];
-				// 	} else if (userLoginInfo['username'] == savedUserInfo['username'] && userLoginInfo['password'] != savedUserInfo['password']) {
-				// 		console.log('Password is wrong');
-				// 	} else {
-				// 		console.log('Entered username does not exist ');
-				// 	}
-				// });
-
 				// solution 2  working
 				readLineFromFile('users.txt').then((lines) => {
 					// console.log("lines :",JSON.parse(lines[0]))
 					var usersDict = new Array();
-					for (var singleLine of lines){
-						usersDict.push(JSON.parse(singleLine))
+					for (var singleLine of lines) {
+						usersDict.push(JSON.parse(singleLine));
 					}
-					// console.log("user dictionary :",usersDict[0].username)
-					
 					let userExists = false;
 					let currentUser: string;
 					usersDict.forEach((savedUserInfo) => {
@@ -136,6 +135,46 @@ getUserSelection().then((selection: any) => {
 							console.log('welcome home', userLoginInfo['username']);
 							currentUser = userLoginInfo['username'];
 							userExists = true;
+							getLoggedUserSelection().then((selection: any) => {
+								LoggedUserSelection = selection;
+								switch (LoggedUserSelection['selection']) {
+									case 'display': {
+										readLineFromFile('tasks.txt').then((lines) => {
+											var usersData = new Array()
+											for(var line of lines){
+												usersData.push(JSON.parse(line))
+											}
+											usersData.forEach((userData)=> {
+												if(userData['username'] == currentUser){
+													// console.log(userData['tasks'])
+													console.log("tasks for logged in user ", currentUser)
+													var tasks = userData['tasks'].split(',')
+													tasks.forEach((task,index) => {
+														console.log(`${index+1}- ${task}`)
+													});
+												}
+
+											})
+											
+										});
+
+										break;
+									}
+									case 'add': {
+										console.log('add');
+										break;
+									}
+									case 'remove': {
+										console.log('remove');
+										break;
+									}
+									default: {
+										console.log('wrong choice obviously!!');
+										break;
+									}
+								}
+							});
+
 							return;
 						} else if (userLoginInfo['username'] == savedUserInfo['username'] && userLoginInfo['password'] != savedUserInfo['password']) {
 							console.log('Password is wrong');
@@ -147,7 +186,6 @@ getUserSelection().then((selection: any) => {
 						console.log('Entered username does not exist ');
 					}
 				});
-				
 
 				// solution 1 working
 
